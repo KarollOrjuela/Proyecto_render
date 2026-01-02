d873a94a9a18a0e5cb795d07926f4a9bdb9b61c4
KarollOrjuela <222650852+KarollOrjuela@users.noreply.github.com>
2026-01-02 15:22:23 -0500

    Add comment to ProjectViewSet serializer_class

--- project/api.py
+++ project/api.py
@@ -2,7 +2,8 @@ from .models import Project
 from rest_framework import viewsets,permissions
 from .serializers import ProjectSerializer
 
+# vista principal donde se renderiza los datos dentro del modelo
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     permission_classes= [permissions.AllowAny]
-    serializer_class = ProjectSerializer
+    serializer_class = ProjectSerializer # serializador
