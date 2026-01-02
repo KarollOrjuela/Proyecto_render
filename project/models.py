b842d15d7fb693c1951210a67fe2a3b541ff3764
KarollOrjuela <222650852+KarollOrjuela@users.noreply.github.com>
2026-01-02 15:21:23 -0500

    Add comment for Project model in models.py

--- project/models.py
+++ project/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# Modelo principal donde se guardarán los datos ingresados
 class Project(models.Model):
     Title = models.CharField(max_length= 50)
     Description = models.TextField()
